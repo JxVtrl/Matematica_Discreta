278271db692346cf9be1c1f14698bf24a009fe8b
Joao <joaoviniciusvitral@hotmail.com>
2022-03-29 20:49:54 -0300

    features

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const table = document.querySelector('.table')
-const expressao = "A -> (B -> C) <-> (A ^ B) -> C"
-const expressaoHtml = document.querySelector('.expressao')
-expressaoHtml.innerHTML = expressao
+const table_head = document.querySelector('.table-head')
+var expressao = "A -> (B -> C) <-> (A ^ B) -> C"
 var expressaoSaved = ''
+const expressaoHtml = document.querySelector('.expressao_show')
+const expressao_input = document.querySelector('#text')
+const butao_enviar = document.querySelector('#calc_btn')
+
+butao_enviar.addEventListener('click', () => {
+    if (expressao_input.value != '') {
+        expressao = expressao_input.value   
+        Format()
+    }
+})
 
 //1 - [A] [0] [A!0] [A <-> A!0]
 
@@ -37,6 +46,9 @@ var expressaoSaved = ''
 Format()
 
 function Format() {
+    // Imprimindo no Html
+    expressaoHtml.innerHTML = expressao
+
     // tirando os espacoes das strings de expressao
     const expressaoSemEspacos =
         expressao.replace(/\s/g, '')
@@ -58,28 +70,19 @@ var letras,
 var expressoesComColchete = []
 
 function Variavel(expressao) {
-    const letrasPossiveis = expressao.match(/[A-Z01]/g)
-
-    letras = letrasPossiveis.filter(
-        (value, index, self) => self.indexOf(value) === index)
-
-      
-    console.log(expressao)
-
-
-
-
-
+    var letrasBruto = expressao.match(/[A-Z01]/g)
+    letras = [...new Set(letrasBruto)];
+    
     // Colchete
     if (expressao.includes('[')) {
         // salvar o que estiver dentro de []
         expressoesComColchete = expressao.match(/\[(.*?)\]/g)
-        console.log(expressoesComColchete)
-    
     }
 
     // remove expressaoDoColchete da expressao
     expressao = expressao.replace(expressoesComColchete, '')
+
+    console.log('Expressão: ' + expressao)
 
     // Partes da expressão
     expressaoSeparada = expressao.split(/(<->)/g)
@@ -97,51 +100,85 @@ function Variavel(expressao) {
             expressaoSeparada.splice(expressaoSeparada.indexOf(item), 1)
         }
     })
-    
+
     console.log(expressaoSeparada)
 
     Imprime()
 }
 
 
-function removeColchetes(expressao) {
+function FormatExpression(expressao) {
     return expressao.replace(/\[/g, '').replace(/\]/g, '')
 }
 
+function criarHead() {
+    var headerArray = []
 
-function Imprime() {
-    var colunas = ``
-        
-    // adicionar colunas de letras
-    letras.map(letra => {
-        colunas += `<th>${letra}</th>`
-    })
+    for (let i = 0; i < letras.length; i++) {
+        headerArray.push(letras[i])
+    }
     
     for (let i = 0; i < expressoesComColchete.length; i++) {
-        colunas += `<th>${removeColchetes(expressoesComColchete[i])}</th>`
+        headerArray.push(FormatExpression(expressoesComColchete[i]))
     }
     
     // adicionar colunas de itens separados
     expressaoSeparada.map(item => {
-        colunas += `<th>${item}</th>`
+        headerArray.push(item) 
     })
     
+    // adicionar expressao completa
+    headerArray.push(expressaoSaved)
+
+    // imprimir header
+    headerArray.map(item => {
+        return table_head.innerHTML += `<th>${item}</th>`
+    })
+
+    console.log(headerArray)
+}
+
+function testes() {
+    // calcular numero de possibilidades
+    var qtd = 0
+    for (let i = letras.length; i > 0; i--) qtd += i
+    console.log(qtd)
+
+    let possibilidades = []
+
+    for (let i = 0; i < (1 << letras.length); i++) {
+        let boolArr = [];
+        // Criar array booleano
+        for (let j = letras.length - 1; j >= 0; j--) boolArr.push(Boolean(i & (1 << j)));
+
+        // transformar array booleano em binario
+        for (let k = 0; k < boolArr.length; k++) boolArr[k] = boolArr[k] ? 1 : 0;
+
+        possibilidades.push(boolArr);
+    }
+
+    console.log(possibilidades)
+}
+
+
+
+
+function Imprime() {
+    criarHead()
+
+    testes()
     
-    
-    // adicionar coluna de expressao completa
-    colunas += `<th>${expressaoSaved}</th>`
     
     table.innerHTML += `
         <tr>
-            ${colunas}
+            <td>
+            
+            </td>
         <tr>
-    
-    
-    
     `
 }
 /* Criar uma linha para cada item da função expressaoSeparada, e if(letras == 0 || letras == 1){
-                                                                    expressaoSeparada +=  '1' ou '0'
-                                                                    console.log(expressaoSeparada)
+    expressaoSeparada +=  '1' ou '0'
+    console.log(expressaoSeparada)
 */
 
